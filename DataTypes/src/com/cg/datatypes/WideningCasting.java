ba36532287453f2cb80f7b3958f882b6d37f1bfb
kavya <kavya@DESKTOP-NU93TBK>
2022-06-30 20:28:40 +0530

    commit upload all files

---
+++ DataTypes/src/com/cg/datatypes/WideningCasting.java
@@ -0,0 +1,12 @@
+package com.cg.datatypes;
+
+public class WideningCasting {
+
+	public static void main(String[] args) {
+		int myInt = 6;
+        double myDouble = myInt;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+	}
+
+}
---
+++ DataTypes/src/com/cg/datatypes/main.java
@@ -0,0 +1,11 @@
+package com.cg.datatypes;
+
+public class main {
+
+	public static void main(String[] args) {
+		float d = 6.5f;
+		int a= (int) d ;
+		System.out.println(a);
+	}
+
+}

f51a4547b74d49a971f94fc48e12c1de9d04c9a7
kavya <kavya@DESKTOP-NU93TBK>
2022-07-10 17:59:23 +0530

    commit

---
+++ ControlStatements/src/com/cg/controlstatements/Language.java
@@ -0,0 +1,20 @@
+package com.cg.controlstatements;
+
+public class Language {
+
+	public static void main(String[] args) {
+		   String lang = "Java";
+           if(lang == "Python") {
+        	   System.out.println("Language is Python");
+           }
+           else if (lang == "C++") {  
+               System.out.println("lang is C++");  
+         }
+           else if(lang == "Java") {  
+               System.out.println("lang is Java");    
+           }
+          else {  
+                System.out.println(lang); 
+                }
+         }
+}

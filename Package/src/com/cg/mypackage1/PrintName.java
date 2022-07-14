dab06ad252bab9a20ad2ef7a82087c4b5d842cd5
kavya <kavya@DESKTOP-NU93TBK>
2022-07-14 17:44:43 +0530

    commit

---
+++ Package/src/com/cg/mypackage1/PrintName.java
@@ -0,0 +1,14 @@
+package com.cg.mypackage1;
+
+import com.cg.mypackage.*;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		
+			String name = "Welcome to C2TC Program";
+			MyClass obj = new MyClass();
+			obj.getNames(name);
+			
+	}
+}
